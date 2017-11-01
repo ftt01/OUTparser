e39837300c29c8e62db3a5e5eb9f719f619eb0d3
ftt01 <dallatorre.daniele@gmail.com>
2017-11-01 09:46:48 -0600

    Added main file OUTparser.java

---
+++ src/main/java/org/alterrvista/growworkinghard/jswmm/OUTparser.java
@@ -0,0 +1,23 @@
+package org.alterrvista.growworkinghard.jswmm;
+
+import java.io.File;
+
+public class OUTparser {
+
+    private File outFile;
+
+    public OUTparser(File outFile){
+        this.outFile = outFile;
+    }
+
+    public void setFile(File outFile){
+        this.outFile = outFile;
+    }
+
+    public File getFile(File outFile){
+        return this.outFile;
+    }
+
+    
+    FileInputStream(String name)
+}
